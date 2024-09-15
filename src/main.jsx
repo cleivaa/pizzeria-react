139528804ba0eefb1e54ce2f6f0622d77841dd8d
Cinthia Leiva A <c.leiva.alegria@gmail.com>
2024-09-15 14:10:27 -0300

    se elimina contexto duplicado

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import UserProvider from "./context/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider> 
       <App />
-     </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
